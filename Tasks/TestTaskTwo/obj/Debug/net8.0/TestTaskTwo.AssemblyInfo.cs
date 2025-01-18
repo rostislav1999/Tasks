c2d367b64e734074141d5722d6e7dbad32aff118
rostislav1999 <35755770+rostislav1999@users.noreply.github.com>
2025-01-18 13:42:00 +0400

    Элемент с текстом названия пиццы поместил в тег <a>

--- Tasks/TestTaskTwo/obj/Debug/net8.0/TestTaskTwo.AssemblyInfo.cs
+++ Tasks/TestTaskTwo/obj/Debug/net8.0/TestTaskTwo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestTaskTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bcff51d4eef7211e0bcf58a1d0f040c3d17565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6031d110fc45abae4a49e66ff2bf60f661f29e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestTaskTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestTaskTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
