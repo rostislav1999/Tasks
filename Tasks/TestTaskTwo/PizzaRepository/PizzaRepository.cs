dcd3d855069cc5b32a9205197c391b01bf763284
rostislav1999 <35755770+rostislav1999@users.noreply.github.com>
2025-01-18 09:03:18 +0400

    В репозиторий добавил метод FindById

--- Tasks/TestTaskTwo/PizzaRepository/PizzaRepository.cs
+++ Tasks/TestTaskTwo/PizzaRepository/PizzaRepository.cs
@@ -1,19 +1,36 @@
 using System.Collections.Generic;
+using System.Linq; // Подключаем пространство имен для использования First/FirstOrDefault
 using PizzaApp.Models;
 
 namespace PizzaApp.PizzaRepositories
 {
     public class PizzaRepository
     {
-        public IEnumerable<PizzaModel> GetAllPizzas()
+        private readonly List<PizzaModel> _pizzas;
+
+        public PizzaRepository()
         {
-            return new List<PizzaModel>
+            // Инициализируем список пицц
+            _pizzas = new List<PizzaModel>
             {
                 new PizzaModel(1, "Margherita", 8.99M),
                 new PizzaModel(2, "Pepperoni", 10.99M),
                 new PizzaModel(3, "Hawaiian", 9.99M),
                 new PizzaModel(4, "Veggie", 11.99M)
             };
+        }
+
+        // Метод для получения всех пицц
+        public IEnumerable<PizzaModel> GetAllPizzas()
+        {
+            return _pizzas;
+        }
+
+        // Метод для получения пиццы по ID
+        public PizzaModel FindById(int id)
+        {
+            // Ищем пиццу по ID, если не найдена, возвращается null (через FirstOrDefault)
+            return _pizzas.FirstOrDefault(p => p.Id == id);
         }
     }
 }
